e9d59fcf130194b40c4bf42bdc6196f922a745d0
Castillo Figueroa <macastillo@indracompany.com>
2022-05-20 18:54:56 -0300

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlVhUwXiD99PzJkz8B0l2uinF-TNqggpk",
+  authDomain: "image-project-e25a7.firebaseapp.com",
+  projectId: "image-project-e25a7",
+  storageBucket: "image-project-e25a7.appspot.com",
+  messagingSenderId: "932850775472",
+  appId: "1:932850775472:web:cbabf22d6512d5cb5dd275"
+};
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: firebaseConfig
 };
