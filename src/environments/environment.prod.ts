2b8d737a0d647f004aafd1acd7addfb16ef15604
Castillo Figueroa <macastillo@indracompany.com>
2022-05-22 23:08:30 -0300

    Firebase config production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlVhUwXiD99PzJkz8B0l2uinF-TNqggpk",
-  authDomain: "image-project-e25a7.firebaseapp.com",
-  projectId: "image-project-e25a7",
-  storageBucket: "image-project-e25a7.appspot.com",
-  messagingSenderId: "932850775472",
-  appId: "1:932850775472:web:cbabf22d6512d5cb5dd275"
+  apiKey: "AIzaSyDcRcDojY06Ip2DJTsu0Z-KeFWaFiafZIQ",
+  authDomain: "proyecto-integrador-front.firebaseapp.com",
+  projectId: "proyecto-integrador-front",
+  storageBucket: "proyecto-integrador-front.appspot.com",
+  messagingSenderId: "393283425135",
+  appId: "1:393283425135:web:6d51a87da85ca0a6f70e9a"
 };
 
 export const environment = {
