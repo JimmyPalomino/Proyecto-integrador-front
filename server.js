b0e153396e4adf70dc634066a3a4e54a75ffd824
Castillo Figueroa <macastillo@indracompany.com>
2022-05-22 22:04:39 -0300

    Intento de proxy8

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 //Install express server
 const express = require('express');
 var proxy = require('express-http-proxy');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const path = require('path');
 
 const app = express();
 
+/*
 app.use('/api', proxy('proyecto-dev-full-stack.herokuapp.com', {
     https: true,
     preserveHostHdr: false
 }));
+*/
+
+app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://proyecto-dev-full-stack.herokuapp.com/api',
+      changeOrigin: true,
+    })
+  );
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/proyecto-integrador-front'));
